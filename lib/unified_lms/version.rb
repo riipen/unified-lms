b3387ba77a16887a5a6d17f51ed712610361baf4
mrgutie8 <mrgutie8@asu.edu>
2024-02-03 16:54:09 -0700

    Initial commit

---
+++ lib/unified_lms/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module UnifiedLms
+  VERSION = "0.1.0"
+end
---
+++ spec/unified_lms_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe UnifiedLms do
+  it "has a version number" do
+    expect(UnifiedLms::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
