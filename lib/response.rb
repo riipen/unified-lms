dce766db17e6520b3edf1a35c71f51516892daf3
apassett <apassett@asu.edu>
2024-02-08 15:27:20 -0500

    US#238Task#262 Create the abstract response class for the factory design pattern

---
+++ lib/response.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+require_relative "unified_lms/version"
+
+module UnifiedLms
+  # The Abstract factory builder class for parsing responses from different LMS's
+  # The concrete response builder will parse the reponse from a specified LMS and return
+  # the string representation of the response
+  class Response
+    # To be implemented in all aggregate classes of Response to parse the responce expected by an LMS
+    def parse_response
+      raise NotImplementedError, "#{self.class} has not implemented method '#{__method__}'"
+    end
+  end
+end
