640c196d52acbbc347060659f06c00d795df0254
apassett <apassett@asu.edu>
2024-02-08 15:33:19 -0500

    US#238Task#262 Create the concrete response parser class for blackboard

---
+++ lib/blackboard_response.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require_relative "unified_lms/version"
+
+module UnifiedLms
+  # Concrete response parser class for blackboard API requests
+  class BlackboardResponse < Response
+    # Given the specified parameters parses the response comming from blackboard API
+    def parse_response
+      raise NotImplementedError, "#{self.class} has not implemented method '#{__method__}'"
+    end
+  end
+end
