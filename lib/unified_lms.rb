ce6183670680d17f166dad3c2d435d023686c140
apassett <apassett@asu.edu>
2024-02-12 21:02:26 -0500

    US#238Task#263 Add appropriate error classes

--- lib/unified_lms.rb
+++ lib/unified_lms.rb
@@ -5,4 +5,18 @@ require_relative "unified_lms/version"
 module UnifiedLms
   class Error < StandardError; end
   # Your code goes here...
+
+  class TimeoutError < StandardError; end
+  # Code here...
+
+  class CallNotFoundError < StandardError; end
+  # Code here...
+
+  class LMSNotDefinedError < StandardError; end
+  # Code here...
+
+  class MethodNotDefinedError < StandardError; end
+  # Code here...
+
+
 end
