b998b937e947833b608c958028790c254c605068
Tony Passetti <tony.passetti@aol.com>
2024-02-15 20:39:03 -0500

    US#238Task#263 Back track previous error handling structure

--- lib/unified_lms.rb
+++ lib/unified_lms.rb
@@ -3,20 +3,4 @@
 require_relative "unified_lms/version"
 
 module UnifiedLms
-  class Error < StandardError; end
-  # Your code goes here...
-
-  class TimeoutError < StandardError; end
-  # Code here...
-
-  class CallNotFoundError < StandardError; end
-  # Code here...
-
-  class LMSNotDefinedError < StandardError; end
-  # Code here...
-
-  class MethodNotDefinedError < StandardError; end
-  # Code here...
-
-
 end
