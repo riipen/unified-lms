697dddfed79513c51dc547eb82cc950b164c46ea
apassett <apassett@asu.edu>
2024-02-27 18:16:55 -0500

    US#278Task#279 Ceate student.rb endpoint for blackboard

---
+++ lib/unified_lms/endpoint/blackboard/student.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+# student.rb holds all the api calls for the blackboard student endpoint. This endpoint deals with all methods related
+# to student data
+
+require_relative "version"
+require_relative "error"
