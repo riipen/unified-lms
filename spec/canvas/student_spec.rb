fdc2b371a8410c467f20bba647785b8181c879ed
Geena Rustia <geenarustia@yahoo.com>
2024-02-28 21:20:07 -0800

    Update student_spec.rb
    updated

--- spec/canvas/student_spec.rb
+++ spec/canvas/student_spec.rb
@@ -1,14 +1,23 @@
 require 'spec_helper'
 
-RSpec.describe 'Canvas Student' do
-  describe '#some_method' do
-    it 'does something expected' do
+RSpec.describe CanvasClient do
+  describe '#get_students' do
+    it 'returns a list of students from the Canvas LMS' do
       # Setup
-      student = Student.new
-      result = student.some_method
-
+      canvas_client = CanvasClient.new
+      mock_response = [
+        { 'id' => 1, 'name' => 'John Doe' },
+        { 'id' => 2, 'name' => 'Jane Smith' }
+      ]
+      allow(canvas_client).to receive(:get_students).and_return(mock_response)
+      students = canvas_client.get_students
+      
       # Verify
-      expect(result).to eq(expected_result)
+      expect(students).to be_an(Array)
+      expect(students.length).to eq(2)
+      expect(students.first['name']).to eq('John Doe')
     end
   end
+
+  # More tests
 end
