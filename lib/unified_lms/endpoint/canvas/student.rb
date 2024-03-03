905fe797eade62cf5629d0fcff19140102cfc0b9
Logan Ortiz <loganmortiz@Logans-MBP-2.lan>
2024-03-02 22:49:38 -0500

    Fix deprecated uri

--- lib/unified_lms/endpoint/canvas/student.rb
+++ lib/unified_lms/endpoint/canvas/student.rb
@@ -9,7 +9,8 @@ module UnifiedLms
       # @return [Array<Hash>] Parsed response JSON
       # @see https://canvas.instructure.com/doc/api/all_resources.html#method.courses.students
       def get_students(**params)
-        get("/api/v1/courses/#{params[:course_id]}/students", **params)
+        params = { enrollment_type: "student" }.merge(params)
+        get("/api/v1/courses/#{params[:course_id]}/users", **params)
       end
     end
   end
