5c7a2d66ddc9faa7eccbe8289ae5003c61c549e7
mrgutie8 <mrgutie8@asu.edu>
2024-03-17 22:38:36 -0600

    Task#321 add get students parser

--- lib/unified_lms/parser/canvas/student.rb
+++ lib/unified_lms/parser/canvas/student.rb
@@ -8,8 +8,22 @@ module UnifiedLms
     # @return [Array<Hash>] Parsed response JSON
     # @see https://canvas.instructure.com/doc/api/all_resources.html#method.courses.students
     def self.get_students(data)
-      # TODO: parse get students information
-      puts "get students"
+      main_student_data = []
+
+      data.each do |student|
+        main_student_data << {
+          id: student[:id],
+          name: student[:name],
+          last_name: student[:last_name],
+          first_name: student[:first_name],
+          short_name: student[:short_name],
+          sis_user_id: student[:sis_user_id],
+          email: student[:email],
+        }
+      end
+
+      # Serialize consolidated data
+      render json: main_student_data
     end
   end
 end
