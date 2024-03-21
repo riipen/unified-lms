e87ea8bbb09f5d045da6cd5a2de08cb9db3031c1
Tony Passetti <tony.passetti@aol.com>
2024-03-21 15:21:09 -0400

    US#294Task#307 fix documentation

--- lib/unified_lms/endpoint/blackboard/course.rb
+++ lib/unified_lms/endpoint/blackboard/course.rb
@@ -13,35 +13,6 @@ module UnifiedLms
     module CourseBlackboard
       # Returns a list of courses and organizations.
       #
-      # To view disabled courses a user must have the entitlement 'system.course.VIEW'.
-      #
-      # Users with the 'course.configure-properties.EXECUTE' course entitlement, or the 'system.course.properties.MODIFY' system entitlement can access all course properties.
-      #
-      # Users enrolled in the course have read access to all properties except:
-      #
-      # uuid
-      # externalId
-      # dataSourceId
-      # created
-      # allowGuests
-      # enrollment.accessCode
-      # Users who are not enrolled in the course and do not have associated entitlements only have read access to the following properties:
-      #
-      # id
-      # courseId
-      # name
-      # description
-      # organization
-      # readOnly
-      # termId
-      # availability.available
-      # availability.duration.type
-      # availability.duration.start
-      # availability.duration.end
-      # availability.duration.daysOfUse
-      # When courses are child courses, parent course data is also returned to provide a complete data representation of course relationships.
-      # If the course enrollment is self-enroll, all users can view the course as though they were enrolled.
-      #
       # @param params [Hash] Query string
       # @return [Array<Hash>] Parsed response JSON
       # @see https://developer.blackboard.com/portal/displayApi
