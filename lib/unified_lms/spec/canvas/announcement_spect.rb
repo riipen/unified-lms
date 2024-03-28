c9abd2754389404bffb5756c05314f6d166afd3b
apassett <apassett@asu.edu>
2024-03-27 22:44:24 -0400

    US#295_Task#314 set up boilerplate code for the get_announcements test

---
+++ lib/unified_lms/spec/canvas/announcement_spect.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+describe CanvasClient do
+  describe "get_announcements" do
+    it "Retrieves the announcements for a course" do
+      expect(true).to eq(true)
+    end
+  end
+end
