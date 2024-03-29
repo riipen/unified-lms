57aaa7cb057e732e0887f91d6036f7a005060522
Daniel Rodriguez <danielrodmar@hotmail.com>
2024-03-28 21:19:03 -0500

    Added courses_spec.rb file to new directory

---
+++ spec/canvas/course_spec.rb
@@ -0,0 +1,11 @@
+def test_get_courses
+  expected_courses = ['Mathematics', 'Science', 'History']
+
+  actual_courses = get_courses
+
+  if actual_courses == expected_courses
+    puts "Test Passed: Expected courses match the actual courses."
+  else
+    puts "Test Failed: Expected #{expected_courses.inspect} but got #{actual_courses.inspect}."
+  end
+end
