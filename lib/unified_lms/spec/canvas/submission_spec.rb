61eeb465ff51401ab1c494cae5ca08ac3fe6e91a
Tony Passetti <tony.passetti@aol.com>
2024-04-01 23:24:23 -0400

    US#295Task#317 add boiler plate code for the get_submission test

---
+++ lib/unified_lms/spec/canvas/submission_spec.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+describe CanvasClient do
+  describe "get_submission" do
+    it "Gets a single submission based on course id, assignment id, and user id" do
+
+      expect(true).to eq(true)
+    end
+  end
+end
