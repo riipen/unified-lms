06c61ce30653edb9cc3563630028331e438039ac
Logan Ortiz <loganmortiz@Logans-MBP-2.lan>
2024-04-06 22:39:09 -0400

    Add canvas announcement parser

---
+++ lib/unified_lms/parser/canvas/announcement.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+require 'json'
+
+module UnifiedLms
+  module ParserCanvas
+    # Fetch a paginated list of announcements for the given courses and date range.
+    #
+    # @param params [Hash] Query string
+    # @return [Array<Hash>] Parsed response JSON
+    # @see https://canvas.instructure.com/doc/api/all_resources.html#method.announcements_api.index
+    def self.get_announcements(data)
+      main_announcements_data = []
+
+      JSON.parse(data).each do |announcement|
+        main_announcements_data << {
+          id: announcement[:id],
+          title: announcement[:title],
+          message: announcement[:message],
+          posted_at: announcement[:posted_at]
+        }
+      end
+
+      # Serialize consolidated data
+      render json: main_announcements_data
+    end
+  end
+end
