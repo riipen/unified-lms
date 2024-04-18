0a603123c52540b90fc599dd61f925b06eadf652
Logan Ortiz <loganmortiz@Logans-MBP-2.lan>
2024-04-17 23:52:24 -0400

    Add type params to canvas endpoint files

--- lib/unified_lms/endpoint/canvas/announcement.rb
+++ lib/unified_lms/endpoint/canvas/announcement.rb
@@ -9,7 +9,7 @@ module UnifiedLms
       # @return [Array<Hash>] Parsed response JSON
       # @see https://canvas.instructure.com/doc/api/all_resources.html#method.announcements_api.index
       def get_announcements(**params)
-        get("/api/v1/announcements", **params)
+        get("/api/v1/announcements", :canvas, **params)
       end
     end
   end
