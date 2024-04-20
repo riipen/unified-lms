2ade682338804e2e23488f7ff461516f93fa262e
Logan Ortiz <loganmortiz@Logans-MBP-2.lan>
2024-04-19 21:04:25 -0400

    Add CourseCanvas parser call

--- lib/unified_lms/endpoint/canvas/course.rb
+++ lib/unified_lms/endpoint/canvas/course.rb
@@ -1,15 +1,18 @@
 # frozen_string_literal: true
+require_relative "../../parser/canvas/course"
 
 module UnifiedLms
   module EndpointCanvas
     module CourseCanvas
+      include ParserCanvas
       # Fetch a paginated list of active courses for the current user.
       #
       # @param params [Hash] Query string
       # @return [Array<Hash>] Parsed response JSON
       # @see https://canvas.instructure.com/doc/api/all_resources.html#method.courses.index
       def get_courses(**params)
-        get("/api/v1/courses", **params)
+        data = get("/api/v1/courses", **params)
+        ParserCanvas::get_courses(data)
       end
     end
   end
