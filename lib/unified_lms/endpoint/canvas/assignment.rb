c9af45682a587ddcbe2c9b7ef5e9bd63fe9ee35e
Geena Rustia <geenarustia@yahoo.com>
2024-04-20 15:13:42 -0700

    Task#343_Add_parser_call_endpoint_canvas_assignment

--- lib/unified_lms/endpoint/canvas/assignment.rb
+++ lib/unified_lms/endpoint/canvas/assignment.rb
@@ -1,15 +1,20 @@
 # frozen_string_literal: true
 
+require "json"
+require_relative "../../parser/canvas.assignments"
 module UnifiedLms
   module EndpointCanvas
     module AssignmentCanvas
+      include ParserCanvas
       # Fetch a paginated list of assignments for the course.
       #
       # @param params [Hash] Query string
       # @return [Array<Hash>] Parsed response JSON
       # @see https://canvas.instructure.com/doc/api/all_resources.html#method.assignments_api.index
       def get_assignments(**params)
-        get("/api/v1/courses/#{params[:course_id]}/assignments", **params)
+        data = get("/api/v1/courses/#{params[:course_id]}/assignments", **params)
+        json_data = JSON.parse(data)
+        ParserCanvas.get_assignments(json_data)
       end
     end
   end
