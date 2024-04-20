d08908e7e9f9c0eefe36235b48bf6714ea0e42c0
Logan Ortiz <loganmortiz@Logans-MBP-2.lan>
2024-04-19 21:06:59 -0400

    Add SubmissionCanvas parser call

--- lib/unified_lms/endpoint/canvas/submission.rb
+++ lib/unified_lms/endpoint/canvas/submission.rb
@@ -1,15 +1,18 @@
 # frozen_string_literal: true
+require_relative "../../parser/canvas/submission"
 
 module UnifiedLms
   module EndpointCanvas
     module SubmissionCanvas
+      include ParserCanvas
       # Get a single submission, based on course id, assignment id and user id.
       #
       # @param params [Hash] Query string
       # @return [Array<Hash>] Parsed response JSON
       # @see https://canvas.instructure.com/doc/api/all_resources.html#method.submissions_api.show
       def get_submission(**params)
-        get("/api/v1/courses/#{params[:course_id]}/assignments/#{params[:assignment_id]}/submissions/#{params[:user_id]}", **params)
+        data = get("/api/v1/courses/#{params[:course_id]}/assignments/#{params[:assignment_id]}/submissions/#{params[:user_id]}", **params)
+        ParserCanvas::get_submission(data)
       end
     end
   end
