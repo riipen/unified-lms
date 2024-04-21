b0584985b6bb5ae0451ddf7fffd3b1738d97493d
Daniel Rodriguez <danielrodmar@hotmail.com>
2024-04-21 01:47:00 -0500

    Parses and renders as JSON accounts for different parameters

--- lib/unified_lms/parser/blackboard/course.rb
+++ lib/unified_lms/parser/blackboard/course.rb
@@ -1,24 +1,35 @@
-# frozen_string_literal: true
+require 'json'
 
 module UnifiedLms
   module ParserBlackboard
-    # Fetch a paginated list of courses enrolled in this course.
+    # Parse the response from the Blackboard API.
     #
-    # @param params [Hash] Query string
-    # @return [Array<Hash>] Parsed response JSON
-    # @see https://canvas.instructure.com/doc/api/all_resources.html#method.courses.courses
-    def self.get_courses(data)
-      data = JSON.parse(response)
-      main_course_data = []
+    # @param response [String] JSON response string
+    # @return [String] Serialized JSON response data
+    def self.parse_response(response)
+        data = JSON.parse(response)
+        parsed_data = []
 
-      data["results"].each do |course|
-        main_course_data << {
-          id: course["courseid"],
-        }
+        data["results"].each do |course|
+          parsed_course = {}
+          parsed_course[:courseId] = course["courseId"]
+          parsed_course[:name] = course["name"]
+          parsed_course[:description] = course["description"]
+          parsed_course[:externalId] = course["externalId"]
+          parsed_course[:created] = course["created"]
+          parsed_course[:allowGuests] = course["allowGuests"]
+          parsed_course[:availability] = course["availability"]
+          parsed_course[:dataSourceId] = course["dataSourceId"]
+          parsed_course[:termId] = course["termId"]
+          parsed_course[:organization] = course["organization"]
+          parsed_data << parsed_course
+        end
+
+      render json: parsed_data
+    rescue JSON::ParserError => e
+      # Handle JSON parsing errors
+      puts "Error parsing JSON response: #{e.message}"
+      JSON.generate([])
       end
-
-      # Serialize consolidated data
-      render json: main_course_data
-    end
   end
 end
