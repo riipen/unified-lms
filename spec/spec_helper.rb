d1f9cb49aa138d17d73094b2e3dd3a12cde3679f
mrgutie8 <mrgutie8@asu.edu>
2024-04-25 22:33:39 -0700

    diable web connect

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,6 +5,8 @@ require 'bundler/setup'
 require 'pry'
 require 'webmock/rspec'
 
+WebMock.disable_net_connect!
+
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = ".rspec_status"
