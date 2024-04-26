04b20e3a9c04db22cd5a2cc676593f31b0342f99
apassett <apassett@asu.edu>
2024-04-26 01:16:59 -0400

    Webmock testing fixes

---
+++ spec/canvas/submission_spec.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+require 'webmock'
+
+RSpec.describe UnifiedLms::Client do
+  describe "#get submission" do
+    it "Gets a single submission based on course id, assignment id, and user id" do
+
+      #set up client
+      client = UnifiedLms::Client.create_client(
+    :canvas,
+    redirect_uri: "https://example.com/",
+    client_id: "example_id",
+    client_secret: "example_secret")
+
+      #create the mock responce
+      mock_response = '{
+        "assignment_id": 23,
+        "assignment": null,
+        "course": null,
+        "attempt": 1,
+        "posted_at": "2020-01-02T11:10:30Z",
+        "read_status": "read",
+        "redo_request": true
+      }'
+
+      # make the get submission call
+      submissions = client.get_submission(course_id: 1, assignment_id: 23, user_id: 1)
+
+      # validate
+      expect(submission["assignment_id"]).to eq(23)
+      expect(submission["attempt"]).to eq(1)
+      expect(submission["read_status"]).to eq("read")
+      expect(submission["redo_request"]).to eq("true")
+    end
+  end
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 require "unified_lms"
+require 'bundler/setup'
+require 'pry'
+require 'webmock/rspec'
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
